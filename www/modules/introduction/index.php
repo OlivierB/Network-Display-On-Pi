c46c0eefaa724ed1be8c88e1bbb85a262fae804a
Lulububu <erwan.matrat@cegetel.net>
2013-07-05 18:14:44 +0100

    ajout module par defaut

---
+++ www/modules/introduction/index.php
@@ -0,0 +1,16 @@
+<link rel="stylesheet" type="text/css" href="/modules/introduction/style/style.css">
+<h1>Introduction</h1>
+
+<div class='slide-div'>
+	<div class='height-full tutorial' >
+
+		<h2>Welcome!</h2>
+		<div class="instructions">
+			First you need to go to the <a href="/admin" >admin page !</a>
+		</div>
+		<a href="/admin" >
+			<img id="logoNDOP" src="/modules/introduction/res/logo.png">
+		</a>
+
+	</div>
+</div>
