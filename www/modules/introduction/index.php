c1d3583259bd668497b037b3a4c56d8bec7a2821
Lulububu <erwan.matrat@cegetel.net>
2013-07-08 11:38:23 +0100

    modification logo module introduction

--- www/modules/introduction/index.php
+++ www/modules/introduction/index.php
@@ -4,12 +4,12 @@
 <div class='slide-div'>
 	<div class='height-full tutorial' >
 
-		<h2>Welcome!</h2>
+		<h2>Welcome to NDOP!</h2>
 		<div class="instructions">
 			First you need to go to the <a href="/admin" >admin page !</a>
 		</div>
 		<a href="/admin" >
-			<img id="logoNDOP" src="/modules/introduction/res/logo.png">
+			<img id="logoNDOP" src="/modules/introduction/res/logo2.png">
 		</a>
 
 	</div>
