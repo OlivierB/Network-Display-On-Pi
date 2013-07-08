afac6efb7f31f3cf36a9c2208197484d8483f5dd
Lulububu <erwan.matrat@cegetel.net>
2013-07-08 12:47:23 +0100

    separation map en deux modules :online et offline

--- www/modules/mapOffline/script/map-loader.js
+++ www/modules/mapOffline/script/map-loader.js
@@ -1,8 +1,6 @@
 // Map creation in the div with id='map'
-if(App.offline)
-	var IpMap = new IpLocationMapOffline('map');
-else
-	var IpMap = new IpLocationMapOnline('map');
+var IpMap = new IpLocationMapOffline('map-offline');
+
 
 // Connection to the IP provider programm
 IpMap.connect(dispatcher, 'iplist');
