a29490a2bd6844503b1ddcb65cb4646c05f1a093
Lulububu <erwan.matrat@cegetel.net>
2013-07-09 16:17:34 +0100

    adaptation modification bdd

--- www/admin/app/actions/layout.php
+++ www/admin/app/actions/layout.php
@@ -1,12 +1,12 @@
 <?php
 	$this['layout_page'] = 'active';
 
-	$sql = "SELECT * FROM  `modules` ORDER BY name";
+	$sql = "SELECT * FROM  `module` ORDER BY name";
 
 	$results = $this['database']->query($sql);
 	$modules = $results->fetchAll(PDO::FETCH_ASSOC);
 
-	$sql = "SELECT id, folder_name, name FROM  `layout` JOIN modules ON id_module = id";
+	$sql = "SELECT id, folder_name, name FROM  `layout` JOIN `module` ON id_module = id";
 
 	$results = $this['database']->query($sql);
 	$pages = $results->fetchAll(PDO::FETCH_ASSOC);
