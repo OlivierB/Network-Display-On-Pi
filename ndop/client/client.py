118eebb2467645bdd30a491eb4ab49ccfcd85cd3
OlivierB <olivier.oblin@gmail.com>
2013-07-10 17:18:35 +0100

    create curses window

--- ndop/client/client.py
+++ ndop/client/client.py
@@ -9,6 +9,8 @@ import pprint
 import sys
 import base64
 import json
+import curses
+from threading import Thread
 
 INDENT=2
 
@@ -41,11 +43,12 @@ def on_message(ws, message):
 
 
     if len(p.l_proto) == 0 or proto in p.l_proto:
-        print "#####START"
+        # print "#####START"
         
-        print "PROTO :", proto
-        pp.pprint(data)
-        print "END#######"
+        # print "PROTO :", proto
+        # pp.pprint(data)
+        # print "END#######"
+        pass
 
 def on_error(ws, error):
     # print "Error :",error
@@ -77,23 +80,83 @@ class ProtoMGMT(object):
 
     #  class values
     l_proto = list()
+    all_proto = list()
 
     def add_proto(self, proto):
         self.l_proto.append(proto)
+
+    def set_all_proto(self, list):
+        self.all_proto = list
 
 
 def get_list(addr):
     ws = websocket.create_connection("ws://"+addr+"/admin")
     ws.send("")
-    pp = pprint.PrettyPrinter(indent=INDENT)
+    # pp = pprint.PrettyPrinter(indent=INDENT)
     result =  json.loads(base64.b64decode(ws.recv()))
-    print "Protocols list :"
-    pp.pprint(result)
+    # print "Protocols list :"
+    # pp.pprint(result)
+    p = ProtoMGMT()
+    p.set_all_proto(result)
     ws.close()
+
+class Window(Thread):
+    """
+    Thread class for tornado webserver
+    """
+
+    def __init__(self):
+        Thread.__init__(self)
+
+        # HTTP server
+        self.term = False
+        self.proto = ProtoMGMT()
+
+    def run(self):
+        try:
+            stdscr = curses.initscr()
+            curses.noecho()
+            curses.cbreak()
+            stdscr.keypad(1)
+
+            pad = curses.newpad(100, 100)
+
+            #  These loops fill the pad with letters; this is
+            # explained in the next section
+            for y in range(0, 100):
+                for x in range(0, 100):
+                    try: pad.addch(y,x, ord('a') + (x*x+y*y) % 26 )
+                    except curses.error: pass
+
+            pad.refresh( 0,0, 5,5, 20,75)
+
+
+            while not self.term:
+                time.sleep(1)
+                stdscr.refresh()
+                time.sleep(1)
+                pad.refresh( 0,0, 0,5, 20,75)
+                time.sleep(1)
+                stdscr.refresh()
+                
+                
+        except KeyboardInterrupt:
+            pass
+        except:
+            raise
+        finally:
+            curses.nocbreak(); stdscr.keypad(0); curses.echo()
+            curses.endwin()
+
+
+    def stop(self):
+        self.term = True
+
 
 
 def main():
     p = ProtoMGMT()
+    w = Window()
 
     args = parser().parse_args()
 
@@ -119,15 +182,19 @@ def main():
                                 on_close = on_close)
     ws.on_open = on_open
 
+
     try:
+        w.start()
         ws.run_forever()
     except KeyboardInterrupt:
         print "Stopping..."
+    finally:
+        w.stop()
+
     return 0
 
 
 if __name__ == "__main__":
     sys.exit(main())
     
-
 
