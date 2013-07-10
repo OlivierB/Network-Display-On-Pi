da18a617b0e289e02864a9cf0fe00020b93b87c6
olivierB <olivier.oblin@gmail.com>
2013-07-10 23:25:36 +0200

    curses in client

--- ndop/client/client.py
+++ ndop/client/client.py
@@ -12,12 +12,13 @@ import json
 import curses
 from threading import Thread
 
-INDENT=2
+INDENT = 2
 
 __description__ = "NDOP client"
 __version__ = "0.0.1"
 
 DEF_ADDR = "127.0.0.1:9000"
+
 
 def parser():
     """
@@ -25,37 +26,40 @@ def parser():
     """
     parser = argparse.ArgumentParser()
     # init
-    parser.description = "%s Version %s" % (__description__,__version__)
+    parser.description = "%s Version %s" % (__description__, __version__)
 
     parser.add_argument("protocols", nargs='*', help="select some protocols")
     parser.add_argument("--addr", help="websocket server address", default=("%s" % DEF_ADDR))
     parser.add_argument("--list", action='store_true', help="Protocols list")
-    parser.add_argument('--version', action='version', version=('NDOP %s' %  __version__))
+    parser.add_argument('--version', action='version', version=('NDOP %s' % __version__))
     return parser
+
 
 def on_message(ws, message):
     pp = pprint.PrettyPrinter(indent=INDENT)
     message = json.loads(base64.b64decode(message))
-    proto=message["proto"]
-    data=message["data"]
+    proto = message["proto"]
+    data = message["data"]
 
     p = ProtoMGMT()
 
-
     if len(p.l_proto) == 0 or proto in p.l_proto:
         # print "#####START"
-        
+
         # print "PROTO :", proto
         # pp.pprint(data)
         # print "END#######"
         pass
 
+
 def on_error(ws, error):
     # print "Error :",error
     pass
 
+
 def on_close(ws):
     print "### closed ###"
+
 
 def on_open(ws):
     def run(*args):
@@ -65,7 +69,9 @@ def on_open(ws):
         print "thread terminating..."
     thread.start_new_thread(run, ())
 
+
 class ProtoMGMT(object):
+
     """
     Singleton class to collect client data
     """
@@ -90,17 +96,32 @@ class ProtoMGMT(object):
 
 
 def get_list(addr):
-    ws = websocket.create_connection("ws://"+addr+"/admin")
+    ws = websocket.create_connection("ws://" + addr + "/admin")
     ws.send("")
     # pp = pprint.PrettyPrinter(indent=INDENT)
-    result =  json.loads(base64.b64decode(ws.recv()))
+    result = json.loads(base64.b64decode(ws.recv()))
     # print "Protocols list :"
     # pp.pprint(result)
     p = ProtoMGMT()
     p.set_all_proto(result)
     ws.close()
 
+
+def get_fillpad(size, select=-1):
+    pad = curses.newpad(size, size)
+    p = ProtoMGMT()
+    cnt = 0
+    for elem in p.all_proto:
+        if cnt == select:
+            pad.addstr(cnt, 0, elem, curses.color_pair(1))
+        else:
+            pad.addstr(cnt, 0, elem)
+        cnt += 1
+    return pad
+
+
 class Window(Thread):
+
     """
     Thread class for tornado webserver
     """
@@ -113,45 +134,52 @@ class Window(Thread):
         self.proto = ProtoMGMT()
 
     def run(self):
+        p = ProtoMGMT()
         try:
             stdscr = curses.initscr()
             curses.noecho()
             curses.cbreak()
             stdscr.keypad(1)
+            curses.start_color()
+            curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)
 
-            pad = curses.newpad(100, 100)
+            # Y pad, X pad; Y window, x window, height, width
+            # pad.refresh( 0,0, 5,x, 20,75)
+            pad2 = get_fillpad(25)
 
-            #  These loops fill the pad with letters; this is
-            # explained in the next section
-            for y in range(0, 100):
-                for x in range(0, 100):
-                    try: pad.addch(y,x, ord('a') + (x*x+y*y) % 26 )
-                    except curses.error: pass
-
-            pad.refresh( 0,0, 5,5, 20,75)
-
-
+            x = 0
             while not self.term:
-                time.sleep(1)
+                pad = get_fillpad(25, x)
                 stdscr.refresh()
-                time.sleep(1)
-                pad.refresh( 0,0, 0,5, 20,75)
-                time.sleep(1)
-                stdscr.refresh()
-                
-                
+                pad.refresh(0, 0, 0, 0, 24, 24)
+                # pad2.refresh( 0,0, 0,0, 24,24)
+                stdscr.addstr(2, 40, 'value = %s' % x)
+
+                c = stdscr.getch()
+                stdscr.erase()
+
+                # c = stdscr.getch()
+                if c == ord('p'):
+                    x += 1
+                if c == ord('m'):
+                    x -= 1
+                elif c == ord('q'):
+                    break
+                elif c == curses.KEY_HOME:
+                    pass
+
         except KeyboardInterrupt:
             pass
         except:
             raise
         finally:
-            curses.nocbreak(); stdscr.keypad(0); curses.echo()
+            curses.nocbreak()
+            stdscr.keypad(0)
+            curses.echo()
             curses.endwin()
-
 
     def stop(self):
         self.term = True
-
 
 
 def main():
@@ -160,12 +188,11 @@ def main():
 
     args = parser().parse_args()
 
-    if args.list:
-        try:
-            get_list(args.addr)
-        except:
-            print "Cannot get list"
-        return 0
+    # if args.list:
+    try:
+        get_list(args.addr)
+    except:
+        print "Cannot get list"
 
     for a in args.protocols:
         p.add_proto(a)
@@ -176,12 +203,11 @@ def main():
         print "Listen :", p.l_proto
 
     websocket.enableTrace(False)
-    ws = websocket.WebSocketApp("ws://"+args.addr+"/admin",
-                                on_message = on_message,
-                                on_error = on_error,
-                                on_close = on_close)
+    ws = websocket.WebSocketApp("ws://" + args.addr + "/admin",
+                                on_message=on_message,
+                                on_error=on_error,
+                                on_close=on_close)
     ws.on_open = on_open
-
 
     try:
         w.start()
@@ -196,5 +222,3 @@ def main():
 
 if __name__ == "__main__":
     sys.exit(main())
-    
-
