2acdeca742641bc1df76e70b1687f449070ae01b
OlivierB <olivier.oblin@gmail.com>
2013-07-18 16:58:20 +0100

    collect password

--- ndop/ndop/modules/netmod_http.py
+++ ndop/ndop/modules/netmod_http.py
@@ -34,15 +34,6 @@ class NetModChild(NetModule):
         res = pkt.get_protocol("Ethernet", "IPv4", "TCP", "HTTP")
         if res is not None and res.type != "":
 
-            # header_end = res.payload.find("\n\r")
-            # pos_start = res.payload.find("\n", 0, header_end)
-
-            # head = res.payload[0:pos_start].split(" ")
-            # print head
-            # header = res.payload[pos_start+1:header_end].split("\n")
-            # for h in header:
-            #     print h.split(": ")
-
             if res.type == "POST":
 
                 header_end = res.data.find("\n\r")
@@ -69,14 +60,6 @@ class NetModChild(NetModule):
                         self.last.append(val)
                         if len(self.last) > 1000:
                             self.last.pop(0)
-
-
-
-    def database_save(self, db_class):
-        """
-        Called to save module data in sql database every savetime
-        """
-        return None
 
 
 l_wd = [
