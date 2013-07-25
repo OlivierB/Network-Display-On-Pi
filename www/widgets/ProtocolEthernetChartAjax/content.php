d504ed13e6f80f76ecbcdc7f26c94e333f23db5e
Lulububu <erwan.matrat@cegetel.net>
2013-07-25 13:14:15 +0100

    correction chemin

--- www/widgets/ProtocolEthernetChartAjax/content.php
+++ www/widgets/ProtocolEthernetChartAjax/content.php
@@ -6,6 +6,6 @@
 <script type="text/javascript">
 $(function(){
 	var chart = new StackedColumnChartAjax("<?= $id ?>", "<?= $params['title'] ?>");
-	chart.connect(App.webServerAddress, "/pages/sql_request.php?request=protocol_ethernet&day_before_begin=<?= $params['nb_day'] ?>&day_before_end=0&group=<?= $params['group_by'] ?>", <?= $params['refresh_time'] ?>);
+	chart.connect(App.webServerAddress, "pages/sql_request.php?request=protocol_ethernet&day_before_begin=<?= $params['nb_day'] ?>&day_before_end=0&group=<?= $params['group_by'] ?>", <?= $params['refresh_time'] ?>);
 });
 </script>
