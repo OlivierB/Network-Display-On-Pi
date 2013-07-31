254223a105136459d2a031e8d0ed234f34b9772c
Lulububu <erwan.matrat@cegetel.net>
2013-07-31 15:46:35 +0100

    Adaptation du fonctionnement de base (Snort GUI) en veritable widget

--- www/widgets/BaseAlert/content.php
+++ www/widgets/BaseAlert/content.php
@@ -1,3 +1,24 @@
+
+<?php 
+	if($file = fopen('./widgets/BaseAlert//base/base.conf.ini', 'w')){
+	
+		$str = 'address='.$params['address'].PHP_EOL.
+				'port='.$params['port'].PHP_EOL.
+				'login='.$params['login'].PHP_EOL.
+				'name='.$params['name'].PHP_EOL.
+				'type='.$params['type'].PHP_EOL.
+				'password='.$params['password'];
+
+		fwrite($file, $str);
+	}else{
+		echo 'Cannot write in the file <strong>base.conf.ini</strong>. The owner of the NDOP www/ folder need to be www-data (or the user who run the web server program).';
+	}
+
+
+
+
+
+ ?>
 <span id="<?= $id ?>-alert"></span>
 <div  id="<?= $id ?>"></div>
 
@@ -5,7 +26,7 @@
 <script type="text/javascript">
 $(function(){
 	var base = new BaseAlert('<?= $id ?>');
-	base.connect(App.webServerAddress, "./base/perso/base_stat_alerts.php?caller=&sort_order=last_d", <?= $params['refresh_time'] ?>);
+	base.connect(App.webServerAddress, "./widgets/BaseAlert/base/perso/base_stat_alerts.php?caller=&sort_order=last_d", <?= $params['refresh_time'] ?>);
 });
 </script>
 
