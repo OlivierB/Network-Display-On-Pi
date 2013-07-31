3980c077566bf5e9dee240dbdf27b93314773e36
Lulububu <erwan.matrat@cegetel.net>
2013-07-31 15:53:43 +0100

    adaptation changement de nom de dossier pages => app

--- www/admin/app/bootstrap.php
+++ www/admin/app/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once "../pages/NDOP.php";
+require_once "../app/NDOP.php";
 NDOP::init('../ndop.conf.ini');
 if(isset(NDOP::$app['db'])){
 	Atomik::set('database', NDOP::$app['db']);
