b4da0fda23bb1230680d40abde1aa0f852a1897e
OlivierB <olivier.oblin@gmail.com>
2013-08-27 16:55:02 +0100

    skeleton improvment

--- ndop/ndop/modules/netmod_skeleton.py
+++ ndop/ndop/modules/netmod_skeleton.py
@@ -20,6 +20,10 @@ class NetModChild(NetModule):
 
     def __init__(self, *args, **kwargs):
         NetModule.__init__(self, updatetime=5, savecode=('m', 30), protocol='skeleton', savebdd=True, *args, **kwargs)
+        
+        # module variable embed in configuration file
+        self.myConfigVarible = 10
+        self.add_conf_override("myConfigVarible")
 
     def pkt_handler(self, pkt):
         """
@@ -35,6 +39,48 @@ class NetModChild(NetModule):
 
         pkt is formated with flow class
         """
+
+        # Flow variables :
+        #     dFlows
+        #     dOctets
+        #     dPkts
+        #     dst_as
+        #     dst_mask
+        #     dst_tag
+        #     dstaddr
+        #     dstport
+        #     engine_id
+        #     engine_type
+        #     exaddr
+        #     extra_pkts
+        #     first
+        #     in_encaps
+        #     input
+        #     last
+        #     marked_tos
+        #     nexthop
+        #     out_encaps
+        #     output
+        #     peer_nexthop
+        #     prot
+        #     router_sc
+        #     src_as
+        #     src_mask
+        #     src_tag
+        #     srcaddr
+        #     srcport
+        #     sysUpTime
+        #     tcp_flags
+        #     tos
+        #     unix_nsecs
+        #     unix_secs
+
+
+    # Flowtools exception
+    # import flowtools
+    # except flowtools.Error:
+
+
         pass
 
     def update(self):
@@ -43,14 +89,31 @@ class NetModChild(NetModule):
 
         Return values to send to clients (websockets)
         automatically convert in json
+
+        You have to return a dict or None if nothing
         """
+
         return None
 
     def database_init(self, db_class):
         """
         Clalled to init module database
         """
+
+#         req = \
+# """
+# CREATE TABLE IF NOT EXISTS `skeleton_example` (
+#   `id` int(11) NOT NULL AUTO_INCREMENT,
+#   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+#   `text` varchar(60) NOT NULL,
+#   `value` int(11) NOT NULL,
+#   PRIMARY KEY (`id`)
+# ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=0 ;
+# """
+#         db_class.execute(req)
+
         pass
+
 
     def database_save(self, db_class):
         """
@@ -60,5 +123,13 @@ class NetModChild(NetModule):
             else return None
 
         """
-        pass
 
+        # # Create a SQL request
+        # date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        # req = "INSERT INTO skeleton_example(date, text, value) VALUES (\"" + sql_date + "\", \"test\", 0);"
+
+        # # execute SQL
+        # db_class.execute(req)
+
+
+        pass
