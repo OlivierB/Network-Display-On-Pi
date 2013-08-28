d1768fb25dfcf6b81eab44d57de24cfeb1298220
Lulububu <erwan.matrat@cegetel.net>
2013-08-28 15:38:18 +0100

    ajout possibilite choix du nom de bdd + suppression de la config de freegeoip

--- www/admin/app/actions/sql/config_NDOP_database.php
+++ www/admin/app/actions/sql/config_NDOP_database.php
@@ -2,25 +2,30 @@
 /**
  * Called by the config page to save the NDOP database info to the database file.
  **/
-if(isset($_POST['ndop_database_address']) 
-	&& isset($_POST['ndop_database_port']) 
+if(isset($_POST['ndop_database_address'])  
 	&& isset($_POST['ndop_database_login']) 
 	&& isset($_POST['ndop_database_password']))
 {
+	if($_POST['ndop_database_port'] == ''){
+			$_POST['ndop_database_port'] = '3306';
+	}
+	// $ndop_database_name = $data_server_infos['database_name'];
+	if($_POST['ndop_database_name'] == ''){
+			$_POST['ndop_database_name'] = 'NDOP';
+	}
 	
-	$sql = "INSERT INTO  `server_information` (`id` ,`name` ,`ip` ,`port`, `login`, `password`)
+	$sql = "INSERT INTO  `server_information` (`id` ,`name` ,`ip` ,`port`, `login`, `password`, `database_name`)
 		VALUES (3 ,  'ndop_database',  '".
 			$_POST['ndop_database_address']."', '".
 			$_POST['ndop_database_port']."', '".
 			$_POST['ndop_database_login']."', '".
-			$_POST['ndop_database_password']."' )  
-			ON DUPLICATE KEY UPDATE `port`=VALUES(`port`), `ip`=VALUES(`ip`), `login`=VALUES(`login`), `password`=VALUES(`password`);";
-// echo $sql;
+			$_POST['ndop_database_password']."', '".
+			$_POST['ndop_database_name']."' )  
+			ON DUPLICATE KEY UPDATE `port`=VALUES(`port`), `ip`=VALUES(`ip`), `login`=VALUES(`login`), `password`=VALUES(`password`), `database_name`=VALUES(`database_name`);";
+
 	$this['database']->exec($sql);
 	$this->redirect('config');
 	
-}else{
-	echo 45;
 }
 
 
